2c25d5e5761dd1155b28f22ccd416e01d1277c90
geunmoon <geunmooon@gmail.com>
2025-06-26 19:29:43 +0900

    feat: /

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -18,9 +18,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Todos.as_view()),
+    path('/', views.Todos.as_view()),
     path('<int:user_id>/', views.Todos.as_view()),
     path('<int:user_id>/<int:todo_id>/', views.TodoDetail.as_view()),
-    path('<int:user_id>/<int:todo_id>/check', views.TodoCheck.as_view()),
-    path('<int:user_id>/<int:todo_id>/reviews', views.TodoReview.as_view()),
+    path('<int:user_id>/<int:todo_id>/check/', views.TodoCheck.as_view()),
+    path('<int:user_id>/<int:todo_id>/reviews/', views.TodoReview.as_view()),
 ]
