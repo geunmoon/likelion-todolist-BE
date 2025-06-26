3cab1d8602061c6a0f8ab3b7f88b21063a9485d7
geunmoon <geunmooon@gmail.com>
2025-06-26 18:53:37 +0900

    feat: 임시 테스트 cors allow all origins

--- todolistProject/settings.py
+++ todolistProject/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://ec2-52-78-182-61.ap-northeast-2.compute.amazonaws.com:8000"
